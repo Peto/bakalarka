b92bc997a31a730dff37bc9a48b103ba248dd918
Peto <peter.kotulic@gmail.com>
2013-03-15 12:02:12 +0100

    schema DB

---
+++ code/app/Config/Schema/schema.php
@@ -0,0 +1,105 @@
+<?php 
+class AppSchema extends CakeSchema {
+
+	public function before($event = array()) {
+		return true;
+	}
+
+	public function after($event = array()) {
+	}
+
+	public $categories = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 45, 'collate' => 'utf8_slovak_ci', 'charset' => 'utf8'),
+		'user_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'key' => 'index'),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'user_id_idx' => array('column' => 'user_id', 'unique' => 0)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_slovak_ci', 'engine' => 'InnoDB')
+	);
+
+	public $imports = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'date_from' => array('type' => 'date', 'null' => true, 'default' => null),
+		'date_to' => array('type' => 'date', 'null' => true, 'default' => null),
+		'xml_file' => array('type' => 'binary', 'null' => true, 'default' => null),
+		'processed' => array('type' => 'boolean', 'null' => true, 'default' => null),
+		'filename' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 45, 'collate' => 'utf8_slovak_ci', 'charset' => 'utf8'),
+		'user_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'key' => 'index'),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'user_id_imports_idx' => array('column' => 'user_id', 'unique' => 0)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_slovak_ci', 'engine' => 'InnoDB')
+	);
+
+	public $subcategories = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 45, 'collate' => 'utf8_slovak_ci', 'charset' => 'utf8'),
+		'category_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'key' => 'index'),
+		'user_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'index'),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'category_id_idx' => array('column' => 'category_id', 'unique' => 0),
+			'user_id' => array('column' => 'user_id', 'unique' => 0)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_slovak_ci', 'engine' => 'InnoDB')
+	);
+
+	public $transaction_types = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 45, 'collate' => 'utf8_slovak_ci', 'charset' => 'utf8'),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_slovak_ci', 'engine' => 'InnoDB')
+	);
+
+	public $transactions = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'transaction_type_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'key' => 'index'),
+		'name' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 45, 'collate' => 'utf8_slovak_ci', 'charset' => 'utf8'),
+		'amount' => array('type' => 'float', 'null' => true, 'default' => null),
+		'category_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'key' => 'index'),
+		'subcategory_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'key' => 'index'),
+		'user_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'key' => 'index'),
+		'original_transaction_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'key' => 'index'),
+		'post_date' => array('type' => 'date', 'null' => true, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'transaction_type_id_idx' => array('column' => 'transaction_type_id', 'unique' => 0),
+			'category_id_idx' => array('column' => 'category_id', 'unique' => 0),
+			'subcategory_id_idx' => array('column' => 'subcategory_id', 'unique' => 0),
+			'user_id_idx' => array('column' => 'user_id', 'unique' => 0),
+			'original_transaction_id_idx' => array('column' => 'original_transaction_id', 'unique' => 0)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_slovak_ci', 'engine' => 'InnoDB')
+	);
+
+	public $user_types = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 45, 'collate' => 'utf8_slovak_ci', 'charset' => 'utf8'),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_slovak_ci', 'engine' => 'InnoDB')
+	);
+
+	public $users = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'email' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 45, 'key' => 'unique', 'collate' => 'utf8_slovak_ci', 'charset' => 'utf8'),
+		'name' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 45, 'collate' => 'utf8_slovak_ci', 'charset' => 'utf8'),
+		'surname' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 45, 'collate' => 'utf8_slovak_ci', 'charset' => 'utf8'),
+		'password' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_slovak_ci', 'charset' => 'utf8'),
+		'active' => array('type' => 'boolean', 'null' => true, 'default' => '1'),
+		'user_type_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'key' => 'index'),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'email_UNIQUE' => array('column' => 'email', 'unique' => 1),
+			'user_type_id_idx' => array('column' => 'user_type_id', 'unique' => 0)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_slovak_ci', 'engine' => 'InnoDB')
+	);
+
+}
